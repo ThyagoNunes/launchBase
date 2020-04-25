ede78c7ff0e0f5a0ce5fca8085ceff8e625e5a40
thyago nunes <devthyagonunes@gmail.com>
2020-04-25 00:07:55 -0300

    print array inside object inside array again

--- fase02/challenge03/usersAndTechnologies.js
+++ fase02/challenge03/usersAndTechnologies.js
@@ -1,9 +1,11 @@
 const usuarios = [
-    { nome: 'Thyago', tecnologias: ['javaScript','node Js'],},
-    { nome: 'Marcela', tecnologias: ['CSS','HTML','JavaScript'],},
-    { nome: 'Fulano', tecnologias: ['ruby','php'], },
+  { nome: "Thyago", tecnologias: ["javaScript", "node Js"] },
+  { nome: "Marcela", tecnologias: ["CSS", "HTML", "JavaScript"] },
+  { nome: "Fulano", tecnologias: ["ruby", "php"] },
 ];
 
-for ( let usuario of usuarios ){
-    console.log(` ${usuario.nome} trabalha com ${usuario.tecnologias.join(', ')}`)
+for (let usuario of usuarios) {
+  console.log(
+    ` ${usuario.nome} trabalha com ${usuario.tecnologias.join(", ")}`
+  );
 }
