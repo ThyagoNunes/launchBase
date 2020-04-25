51befc8cad0b6febbea443cccf3e60d6fdf31903
thyago nunes <devthyagonunes@gmail.com>
2020-04-24 23:51:18 -0300

    imgCalculator and retirementCalculator

--- fase02/challenge01/imgCalculator.js
+++ fase02/challenge01/imgCalculator.js
@@ -1,14 +1,13 @@
-const nome = 'Thyago';
+const nome = "Thyago";
 const peso = 104;
-const altura = 1.90;
-const sexo = 'Masculino';
+const altura = 1.9;
+const sexo = "Masculino";
 
-const imc = peso / ( Math.pow(altura,2) );          // Math.pow(x,elevadoNoX)
-console.log(imc.toFixed(2));                        // toFixed(2) fixar apenas 2 casas decimais após a vírgula
+const imc = peso / Math.pow(altura, 2); // Math.pow(x,elevadoNoX)
+console.log(imc.toFixed(2)); // toFixed(2) fixar apenas 2 casas decimais após a vírgula
 
-if(imc>=30){
-    console.log(`${nome} você está acima do peso `);    // template string
-}
-else{
-    console.log(`${nome} você não está acima do peso`);
+if (imc >= 30) {
+  console.log(`${nome} você está acima do peso `); // template string
+} else {
+  console.log(`${nome} você não está acima do peso`);
 }
--- fase02/challenge01/retirementeCalculator.js
+++ fase02/challenge01/retirementeCalculator.js
@@ -1,43 +1,40 @@
-const nome = 'Thyago Nunes';
-const sexo = 'Ferminino';
+const nome = "Thyago Nunes";
+const sexo = "Ferminino";
 const idade = 55;
 const anosDeContribuicao = 30;
 
 const aposentadoriaPorTempoDeContribuicao = idade + anosDeContribuicao;
 
-if(sexo === 'Masculino' ){
-    if(anosDeContribuicao >= 35 && idade >= 60){
-        console.log(`Nome: ${nome}, 
+if (sexo === "Masculino") {
+  if (anosDeContribuicao >= 35 && idade >= 60) {
+    console.log(`Nome: ${nome}, 
 Sexo: ${sexo}. 
 Você tem: ${idade} anos
 Contribuiu: ${anosDeContribuicao} anos. 
 Tem pontuação de: ${aposentadoriaPorTempoDeContribuicao} 
 você pode se aposentar! `);
-    }
-    else{
-        console.log(`Nome: ${nome}, 
+  } else {
+    console.log(`Nome: ${nome}, 
 Sexo: ${sexo}. 
 Você tem: ${idade} anos
 Contribuiu: ${anosDeContribuicao} anos. 
 Tem pontuação de: ${aposentadoriaPorTempoDeContribuicao} 
 você não pode se aposentar! `);
-    }
-}
-else if(sexo === 'Ferminino'){
-    if(anosDeContribuicao >= 30 && idade >= 55){
-        console.log(`Nome: ${nome}, 
+  }
+} else if (sexo === "Ferminino") {
+  if (anosDeContribuicao >= 30 && idade >= 55) {
+    console.log(`Nome: ${nome}, 
 Sexo: ${sexo}. 
 Você tem: ${idade} anos
 Contribuiu: ${anosDeContribuicao} anos. 
 Tem pontuação de: ${aposentadoriaPorTempoDeContribuicao} 
 você pode se aposentar! `);
-    }
-    else{
-        console.log(`Nome: ${nome}, 
+  } else {
+    console.log(`Nome: ${nome}, 
 Sexo: ${sexo}. 
 Você tem: ${idade} anos
 Contribuiu: ${anosDeContribuicao} anos. 
 Tem pontuação de: ${aposentadoriaPorTempoDeContribuicao} 
 você não pode se aposentar! `);
-    }
+  }
 }
