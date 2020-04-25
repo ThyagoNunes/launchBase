54a044ba9f55e78ab241df976315cfe8b6e9bb15
thyago nunes <devthyagonunes@gmail.com>
2020-04-25 00:03:50 -0300

    print a property from object

--- fase02/challenge02/printerObjects.js
+++ fase02/challenge02/printerObjects.js
@@ -1,12 +1,14 @@
-const company = { 
-    name: 'Nunes',
-    color: 'Blue and Red',
-    focus: 'Programming',
-    adress: {
-        street: 'Rua sigismundo gonçalves',
-        number: 98
-    }
-}
-console.log('\n');
-console.log(`The company ${company.name} is located in street ${company.adress.street}, ${company.adress.number}\n`);
+const company = {
+  name: "Nunes",
+  color: "Blue and Red",
+  focus: "Programming",
+  adress: {
+    street: "Rua sigismundo gonçalves",
+    number: 98,
+  },
+};
+console.log("\n");
+console.log(
+  `The company ${company.name} is located in street ${company.adress.street}, ${company.adress.number}\n`
+);
 console.log(company);
