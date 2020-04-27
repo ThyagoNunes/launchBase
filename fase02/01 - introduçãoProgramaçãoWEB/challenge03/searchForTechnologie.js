644e6c004786fa2f40cd1b219ec08b612c81cdcd
thyago nunes <devthyagonunes@gmail.com>
2020-04-26 21:55:41 -0300

    search a determinad tecnologie in user and return name user that domain tecnologie

--- fase02/01 - introduçãoProgramaçãoWEB/challenge03/searchForTechnologie.js
+++ fase02/01 - introduçãoProgramaçãoWEB/challenge03/searchForTechnologie.js
@@ -1,20 +1,43 @@
-const users =  [ 
-    { name: 'thyago', tecnologies: ['JavaScript','nodeJs','react','react native','css','html']},
-    { name: 'marcela', tecnologies: ['JavaScript','nodeJs','react','react native','html']},
-    { name: 'fulano', tecnologies: ['JavaScript','nodeJs','react','react native','css','html']}
+const users = [
+  {
+    name: "thyago",
+    tecnologies: [
+      "JavaScript",
+      "nodeJs",
+      "react",
+      "react native",
+      "css",
+      "html",
+    ],
+  },
+  {
+    name: "marcela",
+    tecnologies: ["JavaScript", "nodeJs", "react", "react native", "html"],
+  },
+  {
+    name: "fulano",
+    tecnologies: [
+      "JavaScript",
+      "nodeJs",
+      "react",
+      "react native",
+      "css",
+      "html",
+    ],
+  },
 ];
 
 // ► Check all users and return TRUE IF condition to satisfy OR FALSE case this condition not satisfy ►
-function checkUserUseTecnologie ( users ) {                 
-    for( let tencologie of users.tecnologies ) {
-        if( tencologie == 'css') return true;
-    }
-    return false;
+function checkUserUseTecnologie(users) {
+  for (let tencologie of users.tecnologies) {
+    if (tencologie == "css") return true;
+  }
+  return false;
 }
 
-for ( let i = 0 ; i < users.length; i++){
-    const checkUser = checkUserUseTecnologie(users[i])
-        if(checkUser){
-            console.log(`${users[i].name} use tecnologie`)
-    }
+for (let i = 0; i < users.length; i++) {
+  const checkUser = checkUserUseTecnologie(users[i]);
+  if (checkUser) {
+    console.log(`${users[i].name} use tecnologie`);
+  }
 }
