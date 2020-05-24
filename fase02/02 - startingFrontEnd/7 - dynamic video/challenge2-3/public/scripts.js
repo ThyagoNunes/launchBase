b308e118e5faab918864e921477ed647afff7131
thyago nunes <devthyagonunes@gmail.com>
2020-05-23 23:35:49 -0300

    adding botton/class maximize and DOM

--- fase02/02 - startingFrontEnd/7 - dynamic video/challenge2-3/public/scripts.js
+++ fase02/02 - startingFrontEnd/7 - dynamic video/challenge2-3/public/scripts.js
@@ -1,16 +1,25 @@
 const modalOverlay = document.querySelector('.modal-overlay')
-const modal = document.querySelector('.modal')
 const cards = document.querySelectorAll('.card')
 
 for (let card of cards) {
     card.addEventListener('click',function(){
         const videoId = card.getAttribute("id")
+        modalOverlay.classList.add('active');
 
-        modalOverlay.classList.add('active')
 
-       if (modal.classList.contains('active') == true ){
-        modalOverlay.classList.remove('active');
-       }
+        if(modalOverlay.classList.contains('active') == true ) {
+            document.querySelector('.maximize-modal').addEventListener('click', function(){
+                modalOverlay.classList.add('active');
+                modalOverlay.classList.add('maximize'); 
+            })
+        }
+
+/*         if(modalOverlay.classList.contains('maximize') == true){
+            document.querySelector('.maximize-modal').addEventListener('click', function(){
+                modalOverlay.classList.remove('maximize');
+            })
+        } */
+
 
         modalOverlay.querySelector('iframe').src = `https://rocketseat.com.br/${videoId}`
     })
@@ -18,8 +27,13 @@ for (let card of cards) {
 
 document.querySelector('.close-modal').addEventListener('click',function(){
     modalOverlay.classList.remove('active');
+    modalOverlay.classList.remove('maximize');
+    modalOverlay.classList.remove('minimize');
 })
 
 document.querySelector('.maximize-modal').addEventListener('click',function(){
-    modalOverlay.classList.contains('maximize');
+    modalOverlay.classList.remove('active');
+    modalOverlay.classList.remove('maximize');
+    modalOverlay.classList.remove('minimize');
 })
+
