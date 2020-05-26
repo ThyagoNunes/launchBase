1bfedf297405bcad83dcd95c6c69281f2b1d9ed5
thyago nunes <devthyagonunes@gmail.com>
2020-05-25 23:49:26 -0300

    This is equal the content 1-creating a server

---
+++ fase02/03 - startingBackEnd/2 - template Engine/server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const nunjucks = require("nunjucks");
+
+const server = express();
+
+server.use(express.static("public"));       /* CSS */
+
+server.set("view engine", "njk");          /* SET ENGINE FOR HTML */
+
+nunjucks.configure("views", {               /* CONFIGURE NUNJUCKS */
+  express: server
+});
+
+server.get("/", function (req, res) {
+  return res.render("about");               /* RENDER INDEX */
+});
+
+server.get("/portfolio", function (req, res) {
+  return res.render("portfolio");
+});
+
+
+server.listen(5000, function () {
+  console.log("Server is running");
+});
+
+
