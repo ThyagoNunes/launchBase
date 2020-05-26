eb54c9a2e36a4750d4983d8a8aa60d5d02d81de7
thyago nunes <devthyagonunes@gmail.com>
2020-05-26 02:47:03 -0300

    data.js + modules.exports + layout.njk

---
+++ fase02/03 - startingBackEnd/3 - using dynamic data/server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const nunjucks = require("nunjucks");
+
+const server = express();
+const videos = require("./data")
+
+server.use(express.static("public"));       /* CSS */
+server.set("view engine", "njk");          /* SET ENGINE FOR HTML */
+
+nunjucks.configure("views", {               /* CONFIGURE NUNJUCKS */
+  express: server
+});
+
+server.get("/", function (req, res) {
+  return res.render("about");               /* RENDER INDEX */
+});
+
+server.get("/portfolio", function (req, res) {
+
+  return res.render("portfolio", { items: videos });
+});
+
+
+server.listen(3200, function () {
+  console.log("Server is running");
+});
+
+
