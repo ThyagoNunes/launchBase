71bfdf597c4d1fddd531fb958ab330ca21eef3eb
thyago nunes <devthyagonunes@gmail.com>
2020-05-30 02:15:03 -0300

    get ID page & redirect with window.location.href :computer:

--- fase02/03 - startingBackEnd/5 - single video page/challenge3-3-page-description-course.md/public/script.js
+++ fase02/03 - startingBackEnd/5 - single video page/challenge3-3-page-description-course.md/public/script.js
@@ -1,49 +1,10 @@
-const modalOverlay = document.querySelector('.modal-overlay');
-const cards = document.querySelectorAll('.card');
+const modalOverlay = document.querySelector('.modal-overlay')
+const cards = document.querySelectorAll('.card')
 
-for ( let card of cards ){
-    card.addEventListener('click',function(){
-        const course = card.getAttribute("id")
-        modalOverlay.classList.add('active');
-
-        if(modalOverlay.classList.contains('active') == true){
-            document.querySelector('.maximize-modal').addEventListener('click', function(){
-                modalOverlay.classList.add('maximize')  
-            })
-           
-        }
-
-        if(modalOverlay.classList.contains('active') == true){
-            document.querySelector('.maximize-modal').addEventListener('click', function(){
-                modalOverlay.classList.add('maximize')  
-                    document.querySelector('.close-modal').addEventListener('click', function(){
-                        modalOverlay.classList.remove('maximize')
-                        modalOverlay.classList.remove('active')  
-                    })
-            })
-            document.querySelector('.minimize-modal').addEventListener('click', function(){
-                modalOverlay.classList.add('minimize')    
-            })
-        }
-        modalOverlay.querySelector('iframe').src = `https://rocketseat.com.br/${course}`
+for ( let card of cards ) {
+    card.addEventListener('click', function(){
+        const courseId = card.getAttribute("id")
+        window.location.href = `/${courseId}`
     })
 }
 
-
-document.querySelector('.close-modal').addEventListener('click', function(){
-    modalOverlay.classList.remove('active');
-    modalOverlay.classList.remove('maximixe');
-    modalOverlay.classList.remove('minimize');
-})
-
-document.querySelector('.maximize-modal').addEventListener('click', function(){
-    modalOverlay.classList.remove('maximize');
-    modalOverlay.classList.remove('active');
-    modalOverlay.classList.remove('minimize');
-})
-
-document.querySelector('.minimize-modal').addEventListener('click', function(){
-    modalOverlay.classList.remove('minimize');
-    modalOverlay.classList.remove('active');
-    modalOverlay.classList.remove('maximixe');
-})
