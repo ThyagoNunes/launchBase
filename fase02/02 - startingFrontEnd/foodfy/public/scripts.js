155802f33c655c069eadd731eb1c5d68e2fe9255
thyago nunes <devthyagonunes@gmail.com>
2020-06-01 01:36:24 -0300

    function click return img from recipes

--- fase02/02 - startingFrontEnd/foodfy/public/scripts.js
+++ fase02/02 - startingFrontEnd/foodfy/public/scripts.js
@@ -5,8 +5,7 @@ for (let recipe of recipes) {
     recipe.addEventListener('click', function(){
         const getId = recipe.getAttribute("id")
         modalOverlay.classList.add('active');
-       /*  document.getElementById("img").src = `${getId}.png`
-        document.write("<img src=/assets/`${getId}` ") */
+        modalOverlay.querySelector('iframe').src = `${getId}`
     })
 }
 
