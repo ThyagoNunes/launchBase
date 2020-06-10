ac087d93dc328332edeb66afc609477050767b94
thyago nunes <devthyagonunes@gmail.com>
2020-06-09 23:59:05 -0300

    issues

--- fase02/02 - startingFrontEnd/foodfy/public/scripts.js
+++ fase02/02 - startingFrontEnd/foodfy/public/scripts.js
@@ -4,12 +4,15 @@ const recipes = document.querySelectorAll('.card-recipe')
 for (let recipe of recipes) {
         recipe.addEventListener('click', function(){
         const getPhoto = recipe.getAttribute("id")
-/*      const nameFood = recipe.querySelector('.title')
-        const makeFor = recipe.querySelector('.make')  */
+        const nameFood = recipe.querySelector('.title')
+        const makeFor = recipe.querySelector('.make')
+
         modalOverlay.classList.add('active');
         modalOverlay.querySelector('iframe').src = `${getPhoto}` 
+        
         document.querySelector('.title').innerHTML = `${nameFood}`;
         document.querySelector('.make').innerHTML = `${makeFor}`;
+        document.querySelector('.results').innerHTML = 'Hello World!';
         
     })
 }
