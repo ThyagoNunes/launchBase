716cc2ce8d9e51b03d9030c62c08e4d60e3c964d
thyago nunes <devthyagonunes@gmail.com>
2020-06-17 08:39:16 -0300

    keep trying :D :heart:

--- fase02/03 - startingBackEnd/foodfy/public/scripts.js
+++ fase02/03 - startingBackEnd/foodfy/public/scripts.js
@@ -5,6 +5,7 @@ for ( let recipe of recipes ) {
     recipe.addEventListener("click", function(){
         const recipeId = recipe.getAttribute("id")
         window.location.href = `/recipes/:${recipeId}`
+/*         window.location.href = `/:${recipeId}` */
     })
 }
 
