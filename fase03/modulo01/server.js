443f99e465324d6ae2ea6da4ed7a0abcfbeacbcd
Thyago Nunes <devthyagonunes@gmail.com>
2020-06-19 06:51:45 -0300

    configure all the us for new project launchbase

---
+++ fase03/modulo01/server.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const nunjucks = require("nunjucks");
+
+const server = express();
+const videos = require("./videos")
+const about = require('./about')
+const portfolios = require('./portfolios')
+
+server.use(express.static("public"));      
+server.set("view engine", "njk");          
+
+nunjucks.configure("views", {              
+  express: server, 
+  autoescape: false,
+  noCache: true,
+});
+
+server.get("/", function (req, res) {
+  return res.render("about", { about });
+});
+
+server.get("/portfolio", function (req, res) {
+  return res.render("portfolio", { portfolios });
+});
+
+server.get("/video", function(req, res){
+  const id = req.query.id
+
+  const video = videos.find(function(video){
+    return video.id == id
+  })
+
+  if(!video) { 
+    return res.send("Video not found")
+  }
+
+  return res.render("video", { videos: video })
+})
+
+
+server.use(function(req, res){
+  return res.status(404).render('not-found')
+})
+
+server.listen(process.env.PORT || 3000) 
